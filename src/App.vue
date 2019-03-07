22e9b5221b72f9a010f1ab9455b390c3ef789822
Junseop Ji <gaon0403@gmail.com>
2019-03-07 13:11:18 +0900

    Start of the basic design

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    .title-soo
+      h1 #soOtheIEMChamp
+    el-container
+      el-header
+        h1 Eo "soO" Yoon Soo
+      el-main
+        div(style="display: inline-block;")
+          ul(style="text-align: left;")
+            li 2013 WCS Korea Season 3 준우승자 (vs 백동준, 2:4)
+            li 2014 HOT6 GSL Season 1 준우승자 (vs 주성욱, 3:4)
+            li 2014 HOT6 GSL Season 2 준우승자 (vs 김도우, 2:4)
+            li 2014 HOT6 GSL Season 3 준우승자 (vs 이신형, 2:4)
+            li 2017 HOT6 GSL Season 1 준우승자 (vs 김대엽, 2:4)
+            li 2017 HOT6 GSL Season 2 준우승자 (vs 고병재, 2:4)
+            li 2017 WCS Global Finals 준우승자 (vs 이병렬, 2:4)
+            li 
+              strong IEM Season XIII - Katowice 우승자 (vs 김대엽, 4:2)
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'app',
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./assets/style.less" lang="less">
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/ko';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI, { locale });
 
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  }
+}).$mount('#app');
